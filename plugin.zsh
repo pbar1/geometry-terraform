c454ed8fb8aa36f27d019a2803d6834892e820e8
Pierce Bartine <piercebartine@gmail.com>
2018-07-25 20:28:03 -0500

    added symbol

--- plugin.zsh
+++ plugin.zsh
@@ -1,3 +1,14 @@
+# Color definitions
+GEOMETRY_COLOR_TERRAFORM=${GEOMETRY_COLOR_TERRAFORM:-magenta}
+
+# Symbol definitions
+GEOMETRY_SYMBOL_TERRAFORM=${GEOMETRY_SYMBOL_TERRAFORM:-"▰"}
+GEOMETRY_TERRAFORM=$(prompt_geometry_colorize $GEOMETRY_COLOR_TERRAFORM $GEOMETRY_SYMBOL_TERRAFORM)
+
+prompt_geometry_terraform_workspace() {
+  GEOMETRY_TERRAFORM_WORKSPACE="$(terraform workspace show 2> /dev/null)"
+}
+
 geometry_prompt_terraform_setup() {
   (( $+commands[terraform] )) || return 1
 }
@@ -8,7 +19,9 @@ geometry_prompt_terraform_check() {
 }
 
 geometry_prompt_terraform_render() {
-  echo -n "$(terraform workspace show 2> /dev/null)"
+  prompt_geometry_terraform_workspace
+
+  echo "$GEOMETRY_TERRAFORM $GEOMETRY_TERRAFORM_WORKSPACE"
 }
 
 geometry_plugin_register terraform
